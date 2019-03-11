1cacd29382fe2194b7a28a6a9eccbe065b51cb4b
Mateusz Szostok <szostok.mateusz@gmail.com>
2019-03-11 12:53:23 +0100

    Execute test-unit, format, verify and test-dep

--- pkg/webhook/servicecatalog/servicebroker/mutation/handler_test.go
+++ pkg/webhook/servicecatalog/servicebroker/mutation/handler_test.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package mutation_test
 
 import (
@@ -18,7 +34,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 )
 
-func TestCreateUpdateHandlerHandleSuccess(t *testing.T) {
+func TestCreateUpdateHandlerHandleCreateSuccess(t *testing.T) {
 	tests := map[string]struct {
 		givenRawObj []byte
 
