ddd6833fc69afe5ea55a2328e7bbd9e7bc7b9f26
Walach, Adam <adam.walach@sap.com>
2019-03-22 09:16:54 +0100

    Fix formatting

--- cmd/webhook/server/webhook.go
+++ cmd/webhook/server/webhook.go
@@ -75,7 +75,7 @@ func run(opts *WebhookServerOptions, stopCh <-chan struct{}) error {
 
 		"/mutating-servicebindings":  &sbmutation.CreateUpdateHandler{},
 		"/mutating-servicebrokers":   &brmutation.CreateUpdateHandler{},
-		"/mutating-serviceclasses":     &scmutation.CreateUpdateHandler{},
+		"/mutating-serviceclasses":   &scmutation.CreateUpdateHandler{},
 		"/mutating-serviceinstances": &simutation.CreateUpdateHandler{},
 		"/mutating-serviceplans":     &spmutation.CreateUpdateHandler{},
 	}
